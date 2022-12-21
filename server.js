69eeeadb121c7c398c9c756bfdbe921b252c426f
Patrick Clary <patrickm@theclarys.org>
2022-12-21 11:02:46 -0600

    First Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// Environment Variables
+require('dotenv').config();
+
+// Database Configuration
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Database Connection Error / Success
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Middleware
+
+// Controllers
+
+// Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
